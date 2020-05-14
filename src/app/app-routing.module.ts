7b8454d5c3798fcc1be439e1a100b6f43bf01deb
NevenaJuric <nevenajuric96@gmail.com>
2020-05-14 13:08:26 +0200

    change home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ActivitiesComponent } from './activities/activities.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
   {path: 'activities', component: ActivitiesComponent}
 ];
 
