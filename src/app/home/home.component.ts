37fde56096fbf9ece5140280b398c898376ff454
NevenaJuric <nevenajuric96@gmail.com>
2020-05-14 14:23:53 +0200

    change home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
+import { faFilePdf } from '@fortawesome/free-regular-svg-icons';
+import { faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
+import { faNewspaper } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+
+  faBook = faBook;
+  faFilePdf = faFilePdf;
+  faGlobeAmericas = faGlobeAmericas;
+  faNewspaper = faNewspaper;
 
   constructor() { }
 
