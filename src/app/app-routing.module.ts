20dcc0236e67cfccb9f65bd4202fdd618da50c65
NevenaJuric <nevenajuric96@gmail.com>
2020-05-15 16:05:39 +0200

    add members component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { ActivitiesComponent } from './activities/activities.component';
 import { HomeComponent } from './home/home.component';
 import { MaterialsComponent } from './materials/materials.component';
+import { MembersComponent } from './members/members.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'activities', component: ActivitiesComponent},
-  {path: 'materials', component: MaterialsComponent}
+  {path: 'materials', component: MaterialsComponent},
+  {path: 'members', component: MembersComponent}
 ];
 
 @NgModule({
