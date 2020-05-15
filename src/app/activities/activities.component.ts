8cc819eef01b71ddd0b0d29a11548d13a413a7b5
NevenaJuric <nevenajuric96@gmail.com>
2020-05-15 14:51:12 +0200

    add carausel

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-activities',
-  templateUrl: './activities.component.html'
+  templateUrl: './activities.component.html',
+  providers: [NgbCarouselConfig]
 })
 export class ActivitiesComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
