ce6b1b73feeae4ea97ddf359c237a8cd0b659f2b
NevenaJuric <nevenajuric96@gmail.com>
2020-05-15 15:43:06 +0200

    add materials component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ActivitiesComponent } from './activities/activities.component';
 import { HomeComponent } from './home/home.component';
+import { MaterialsComponent } from './materials/materials.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'activities', component: ActivitiesComponent}
+  {path: 'activities', component: ActivitiesComponent},
+  {path: 'materials', component: MaterialsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './partials/header/header.component';
 import { SmallHeaderComponent } from './partials/small-header/small-header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MaterialsPartialComponent } from './partials/materials-partial/materials-partial.component';
+import { MaterialsComponent } from './materials/materials.component';
 
 @NgModule({
    declarations: [
@@ -27,6 +28,7 @@ import { MaterialsPartialComponent } from './partials/materials-partial/material
       MaterialHomeComponent,
       MaterialsPartialComponent,
       HomeComponent,
+      MaterialsComponent
    ],
    imports: [
       BrowserModule,
