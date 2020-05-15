2fa2810caff472c2af280e4fd11f9f150655f199
NevenaJuric <nevenajuric96@gmail.com>
2020-05-15 13:32:37 +0200

    add small header

---
+++ src/app/partials/small-header/small-header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-header',
+  templateUrl: './small-header.component.html'
+})
+export class SmallHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
