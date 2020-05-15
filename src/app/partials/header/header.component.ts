3adbaf139782367ce97bc984dfed932728ab55b0
NevenaJuric <nevenajuric96@gmail.com>
2020-05-15 12:27:05 +0200

    fixed header component

--- src/app/partials/header/header.component.ts
+++ src/app/partials/header/header.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ParticlesConfig } from '../particles-config';
 
 declare let particlesJS: any;
-declare let particlesJS2: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
@@ -15,7 +14,6 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     this.invokeParticles();
-    this.invokeParticles2();
   }
 
   public invokeParticles(): void {
@@ -23,8 +21,4 @@ export class HeaderComponent implements OnInit {
     particlesJS('scene', ParticlesConfig, function() {});
   }
 
-  public invokeParticles2(): void {
-    // tslint:disable-next-line: only-arrow-functions
-    particlesJS2('layer2', ParticlesConfig, function() {});
-  }
 }
