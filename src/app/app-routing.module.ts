4e07944e3cf9484ab3d2d601ff85117e343e35c4
NevenaJuric <nevenajuric96@gmail.com>
2020-05-18 14:42:46 +0200

    fixed route page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,17 +9,17 @@ import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'activities', component: ActivitiesComponent},
-  {path: 'materials', component: MaterialsComponent},
   {path: 'members', component: MembersComponent},
+  {path: 'materials', component: MaterialsComponent},
+  {path: 'contact', component: ContactComponent},
   {path: 'labors', component: LaborsComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'top'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
