181c09f1093692b98ecb210ebe04245380dd6429
NevenaJuric <nevenajuric96@gmail.com>
2020-05-19 14:32:53 +0200

    add scroll function

--- src/app/partials/navigation/navigation.component.ts
+++ src/app/partials/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
 
 @Component({
   selector: 'app-navigation',
@@ -6,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+  scroll = false;
+
   constructor() { }
 
   ngOnInit() {
+    window.addEventListener('scroll', this.scrolling, true);
   }
 
+  scrolling = (s) => {
+    const sc = s.target.scrollingElement.scrollTop;
+    if (sc >= 50) {
+      this.scroll = true;
+    }
+    else {
+      this.scroll = false;
+    }
+
+  }
 }
