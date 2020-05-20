9ad6076db3e7b992151dd74f3af779070963ad5b
NevenaJuric <nevenajuric96@gmail.com>
2020-05-20 16:31:04 +0200

    add news2 component

---
+++ src/app/news/news2/news2.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news2',
+  templateUrl: './news2.component.html'
+})
+export class News2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
