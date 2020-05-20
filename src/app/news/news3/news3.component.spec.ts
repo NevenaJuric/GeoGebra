f1344f112aef85d8ed603c90321a545c9660b00c
NevenaJuric <nevenajuric96@gmail.com>
2020-05-20 17:57:14 +0200

    add news3 component

---
+++ src/app/news/news3/news3.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { News3Component } from './news3.component';
+
+describe('News3Component', () => {
+  let component: News3Component;
+  let fixture: ComponentFixture<News3Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ News3Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(News3Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/news/news3/news3.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news3',
+  templateUrl: './news3.component.html'
+})
+export class News3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
